2d00cc04c95893a0c1e377cc404edb61dde4bc78
Julia Mendes <juliamendesc@gmail.com>
2024-02-10 14:07:16 +0000

    add link to cart route

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Image, Text, View, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 import colors from 'tailwindcss/colors';
 
 export function Header({
@@ -16,15 +17,17 @@ export function Header({
 				<Text className='text-white text-xl font-heading mt-2'>{title}</Text>
 			</View>
 			{cartQuantityItems > 0 && (
-				<TouchableOpacity className='relative' activeOpacity={0.7}>
-					<View className='bg-lime-300 rounded-full w-4 h-4 justify-center items-center top-2 z-10 -right-3.5'>
-						<Text className='text-slate-900 font-bold text-xs'>
-							{cartQuantityItems}
-						</Text>
-					</View>
+				<Link href='/cart' asChild>
+					<TouchableOpacity className='relative' activeOpacity={0.7}>
+						<View className='bg-lime-300 rounded-full w-4 h-4 justify-center items-center top-2 z-10 -right-3.5'>
+							<Text className='text-slate-900 font-bold text-xs'>
+								{cartQuantityItems}
+							</Text>
+						</View>
 
-					<Feather name='shopping-bag' size={24} color={colors.white} />
-				</TouchableOpacity>
+						<Feather name='shopping-bag' size={24} color={colors.white} />
+					</TouchableOpacity>
+				</Link>
 			)}
 		</View>
 	);
